9b206608af6655cecbc51d9d1958fc183f29ec3e
Milon Chandra Das <milonofficial53@gmail.com>
2024-05-31 10:47:49 +0600

    1st Template

---
+++ Template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define endl "\n"
+#define fli(n) for(int i = 0; i < n; i++)
+#define flj(n) for(int j = 0; i < n; j++)
+#define pb(n) push_back(n)
+
+int main()
+{
+    optimize();
+
+
+    return 0;
+}
