f0fbe5efffbe32941745d8407a58d1b0df46c8a2
Milon Chandra Das <milonofficial53@gmail.com>
2024-06-13 12:35:31 +0600

    June24 Template

--- Template.cpp
+++ Template.cpp
@@ -1,12 +1,22 @@
-#include<bits/stdc++.h>
+// Written by: Apon Das
+// Problem Link:
+// Date:
+#include <bits/stdc++.h>
 using namespace std;
 
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long
 #define endl "\n"
-#define fli(n) for(int i = 0; i < n; i++)
-#define flj(n) for(int j = 0; i < n; j++)
+#define fr(n) for (int i = 0; i < n; i++)
+#define frj(n) for (int j = 0; i < n; j++)
+#define frr(n) for (int i = n; i >= 0; i--)
+#define fe(n) for (auto u : n)
 #define pb(n) push_back(n)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
 
 int main()
 {
