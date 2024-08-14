584ad9e31e4c7b022ac702f6fe45ad7754706312
MilonChandraDas <milonofficial53@gmail.com>
2024-08-14 09:59:18 +0600

    Temp2

--- Template.cpp
+++ Template.cpp
@@ -1,27 +1,35 @@
-// Written by: Apon Das
-// Problem Link:
-// Date:
 #include <bits/stdc++.h>
 using namespace std;
 
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define ll long long
-#define endl "\n"
-#define fr(n) for (int i = 0; i < n; i++)
-#define frj(n) for (int j = 0; i < n; j++)
-#define frr(n) for (int i = n; i >= 0; i--)
-#define fe(n) for (auto u : n)
-#define pb(n) push_back(n)
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
+// Typedefs
+typedef double dl;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<dl> vd;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<dl, dl> pdd;
+typedef pair<ll, ll> pll;
+
+// Defines
+#define fraction(x) cout.unsetf(ios::floatfield); cout.precision(x); cout.setf(ios::fixed, ios::floatfield);
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)x.size()
+#define pf push_front
+#define pb push_back
+#define mp make_pair
+#define nl cout << '\n'
+#define endl '\n'
 #define ff first
 #define ss second
-#define all(v) v.begin(), v.end()
 
 int main()
 {
     optimize();
-
+    
 
     return 0;
 }
